a68421f58015bedacbe4f5c8dafd5f2fd1dec156
Thanh Ta <thanh.ta@shopee.com>
2021-06-23 22:31:17 +0800

    Initialise Xcode project

---
+++ Xcode13/Xcode13/Xcode13App.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode13App.swift
+//  Xcode13
+//
+//  Created by Ta Phuc Thanh on 23/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Xcode13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
