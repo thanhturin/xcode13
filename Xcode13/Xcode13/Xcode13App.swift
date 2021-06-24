41423aa07595706b0780bfb710fadb596e038154
Thanh Ta <thanh.ta@shopee.com>
2021-06-24 21:58:09 +0800

    Add app state callback

--- Xcode13/Xcode13/Xcode13App.swift
+++ Xcode13/Xcode13/Xcode13App.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct Xcode13App: App {
+
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: self.scenePhase) { (newScenePhase) in
+            switch newScenePhase {
+                case .active:
+                    print("App is active")
+
+                case .inactive, .background:
+                    break
+
+                @unknown default:
+                    fatalError()
+            }
+        }
     }
 }
+
